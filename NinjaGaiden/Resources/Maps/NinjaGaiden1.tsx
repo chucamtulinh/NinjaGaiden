27f9165de844ac9f013bb8eaf26b6a10192982bc
Chu Cẩm Tú Linh <chucamtulinh@gmail.com>
2019-04-22 17:06:58 +0700

    Map 3-1

---
+++ NinjaGaiden/Resources/Maps/NinjaGaiden1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="NinjaGaiden1" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="NinjaGaiden1.png" width="160" height="160"/>
+</tileset>
