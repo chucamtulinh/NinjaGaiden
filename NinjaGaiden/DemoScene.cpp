a00c561b7b77bde3ac75c38d1fedb5468d2335d4
quangtritk1998 <45227610+quangtritk1998@users.noreply.github.com>
2019-05-02 11:34:19 +0700

    fixed

--- NinjaGaiden/DemoScene.cpp
+++ NinjaGaiden/DemoScene.cpp
@@ -10,7 +10,7 @@ void DemoScene::LoadContent()
 	//set mau backcolor cho scene o day la mau xanh
 	mBackColor = 0x54acd2;
 
-	mMap = new GameMap("Resources/Maps/NinjaGaiden1.tmx");
+	mMap = new GameMap("Resources/Maps/marioworld1-1.tmx");
 }
 
 void DemoScene::Update(float dt)
